b42be4ef843aaafc7c54cf0a44363d2d4e68a827
akumar <arun.kumar@n-fuse.co>
2024-03-20 11:08:22 +0100

    fix: remove lodash for object equal comparsion

--- handlebars/helper-eq.cjs
+++ handlebars/helper-eq.cjs
@@ -1,6 +1,23 @@
-const _ = require('lodash');
 module.exports = function (hbs, opts) {
-    hbs.registerHelper('eq', function(value1, value2) {
-      return _.isEqual(value1, value2);
-    });
+  hbs.registerHelper('eq', function (obj1, obj2) {
+    // Base case: If both objects are identical, return true.
+    if (obj1 === obj2) {
+      return true;
+    }
+    // Check if both objects are objects and not null.
+    if (typeof obj1 !== 'object' || typeof obj2 !== 'object' || obj1 === null || obj2 === null) {
+      return false;
+    }
+    // Get the keys of both objects.
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    // Check if the number of keys is the same.
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+      return true;
+    }
+    return false;
+  });
 };
